1259c513efa74893047ac69bd28761bebcd33896
Charles Severance <csev@umich.edu>
2018-08-07 12:12:45 -0400

    oar copies of phpmyadmin

--- reset.sh
+++ reset.sh
@@ -16,6 +16,11 @@ echo "Emptying /var/www/html/d"
 rm -rf /var/www/html/d
 mkdir /var/www/html/d
 
+echo Downloading phpMyAdmin
+cd /home/ubuntu
+curl -O https://files.phpmyadmin.net/phpMyAdmin/4.7.9/phpMyAdmin-4.7.9-all-languages.zip
+
+
 for i in $DEV_SERVER_LIST; do
   host=`echo $i | awk -F',' '{print $1}'`
   admin=`echo $i | awk -F',' '{print $2}'`
@@ -62,6 +67,19 @@ cd /var/www/html/d/$host/tsugi/admin
 php upgrade.php
 
 done
+
+echo Setting up phpMyAdmin for $host
+
+cd /home/ubuntu
+X=`sha256sum phpMyAdmin-4.7.9-all-languages.zip | awk '{print $1}'`
+if [ "$X" == "2fb9f7b31ae7cb71f6398e5da8349fb4f41339386e06a851c4444fc7a938a38a" ]
+then
+  echo "Sha Match"
+  unzip phpMyAdmin-4.7.9-all-languages.zip
+  mv phpMyAdmin-4.7.9-all-languages /var/www/html/$d/$host/phpMyAdmin
+else
+  echo "SHA256 mismatch"
+fi
 
 echo "Resetting permissions on /var/www/html"
 chown -R www-data:www-data /var/www/html
