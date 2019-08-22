43630dc9a4135b6fe730fd8e89f38a5fd764a9db
root <root@ip-172-31-47-50.us-east-2.compute.internal>
2019-08-22 14:13:59 +0000

    Fix permissions in the root script

--- cron_extra_root.sh
+++ cron_extra_root.sh
@@ -6,6 +6,8 @@ x=`find /var/www/html/dev -maxdepth 0 -type d -ctime +2 | wc -l`
 echo $x
 if [[ $x > 0  ]] ; then
   echo Doing the reset `date` > /tmp/reset-time.txt
+  chown -R www-data:www-data /var/www/html
   bash /home/ubuntu/dev-setup/reset.sh
+  chown -R www-data:www-data /var/www/html
 fi
 
